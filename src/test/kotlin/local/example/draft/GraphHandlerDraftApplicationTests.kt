ebdbc1787e40baf0c43a6b0c7ea9e7688e749dfc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-11-03 07:43:46 +0100

    Create GraphHandlerDraftApplicationTests.kt

---
+++ src/test/kotlin/local/example/draft/GraphHandlerDraftApplicationTests.kt
@@ -0,0 +1,47 @@
+/**
+ *
+ * Copyright 2018 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.draft
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [GraphHandlerDraftApplication::class])
+@AutoConfigureMockMvc
+class GraphHandlerDraftApplicationTests {
+
+	@Autowired
+	val mockMvc: MockMvc? = null
+
+	@Before
+	@Throws(Exception::class)
+	fun initialize() {
+
+	}
+
+	@Test
+	fun `sample test`() {
+	}
+}
